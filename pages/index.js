9105fe0e93156e948bdf5c761238ac30ea70bfb8
Vinicius <sdvinimendonca@outlook.com>
2026-01-28 21:26:03 -0300

    adiciona pasta pages

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home (){
+    return <h1>Isadora, eu te amo. olha meu sitezinho no ar heheheheh</h1> 
+    return <hr>Ainda nao tem muita coisa mas logo vai ter.</hr>
+    
+}
+
+export default Home;
