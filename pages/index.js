30e6aeac952ae3d635525a721cd90c80cfc7b632
Vinicius <sdvinimendonca@outlook.com>
2026-03-03 22:09:00 -0300

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-function Home (){
-    return <h1>Isadora, eu te amo. olha meu sitezinho no ar heheheheh<br></br>
-    Ainda nao tem nada, mas alguma hora vai ter</h1> 
-    
-    
+function Home() {
+  return (
+    <h1>
+      Isadora, eu te amo. olha meu sitezinho no ar hehehehe ainda nao tem nada,
+      mas alguma hora vai ter OBRIGADO
+    </h1>
+  );
 }
 
 export default Home;
